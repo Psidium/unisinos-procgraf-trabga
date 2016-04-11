31fc2f362afd4d1504f713d56f7f6865e934eae9
Psidium <psiidium@gmail.com>
2016-04-11 20:44:05 -0300

    initial project

---
+++ JogoGA/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  JogoGA
+//
+//  Created by Psidium on 4/11/16.
+//  Copyright © 2016 Psidium. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
